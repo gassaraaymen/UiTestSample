586e8b93be1ceb246f2f511adec45e281eeaa80c
Ayman Gassara <a.gassara@root.sa>
2018-04-23 09:44:28 +0100

    Add project files.

---
+++ UITest/Tests.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using NUnit.Framework;
+using Xamarin.UITest;
+using Xamarin.UITest.Queries;
+
+namespace UITest
+{
+    [TestFixture(Platform.Android)]
+    //[TestFixture(Platform.iOS)]
+    public class Tests
+    {
+        IApp app;
+        Platform platform;
+
+        public Tests(Platform platform)
+        {
+            this.platform = platform;
+        }
+
+        [SetUp]
+        public void BeforeEachTest()
+        {
+            app = AppInitializer.StartApp(platform);
+        }
+
+        [Test]
+        public void AppLaunches()
+        {
+            app.Screenshot("First screen.");
+        }
+
+        [Test]
+        public void Add_5_To_7_Returns_12_UITest()
+        {
+            // Arrange (Prepare Our Environment)
+            app.Screenshot("Add text");
+            app.EnterText("EntryA", "5");
+            app.EnterText("EntryB", "7");
+
+            // Act
+
+            app.Tap("ButtonAdd");
+            app.Screenshot("Result");
+
+            //Assert
+
+            var appResult = app.Query("LabelResult").First(result => result.Text == "12");
+            Assert.IsTrue(appResult != null, "Label is not displaying the right result !");
+        }
+
+
+        [Test]
+        public void Multiply_5_To_7_Returns_35_UITest()
+        {
+            // Arrange (Prepare Our Environment)
+            app.Screenshot("Add text");
+            app.EnterText("EntryA", "5");
+            app.EnterText("EntryB", "7");
+
+            // Act
+
+            app.Tap("ButtonMultiply");
+            app.Screenshot("Result");
+            //Assert
+
+            var appResult = app.Query("LabelResult").First(result => result.Text == "35");
+            Assert.IsTrue(appResult != null, "Label is not displaying the right result !");
+        }
+    }
+}
+
---
+++ UiTestSample/UiTestSample/MainPage.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace UiTestSample
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        private void Add_Clicked(object sender, EventArgs e)
+        {
+            ResultLabel.Text = (Convert.ToInt32(ValueAEntry.Text) + Convert.ToInt32(ValueBEntry.Text)).ToString();
+        }
+
+        private void Multiply_Clicked(object sender, EventArgs e)
+        {
+            ResultLabel.Text = (Convert.ToInt32(ValueAEntry.Text) * Convert.ToInt32(ValueBEntry.Text)).ToString();
+        }
+
+    }
+}
